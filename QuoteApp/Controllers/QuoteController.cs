5d49e514e174b9f992b0b9c456c4264b23d022c6
Алан <xalvashokolad@gmail.com>
2025-06-17 07:23:03 +0300

    Реализовал авторизацию

--- QuoteApp/Controllers/QuoteController.cs
+++ QuoteApp/Controllers/QuoteController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using QuoteApp.Db.Models;
 using QuoteApp.Models;
 using QuoteApp.ViewModels;
